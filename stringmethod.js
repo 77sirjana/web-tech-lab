9d01ca9c7c34dc63bf681a066f07daaca3e800f0
sirjana <tsirjana059@gmail.com>
2024-06-06 08:20:31 +0545

    string

---
+++ stringmethod.js
@@ -0,0 +1,16 @@
+var text="iam currently involved in bsc.csit 5th sem."
+//alert(text);
+console.log(text);
+//print the length of string
+console.log("the length of text is:",
+    text.length);
+    //print the character at index 5 of the given strig
+console.log("the character at index 5 is:",text.charAt(5));
+//print the character at index 5 of the given strig
+console.log("the character at index 5 is:",text.charCodeAt(5));
+console.log("the character at index 5 using string[] is :",text[5]);
+//print and write it into the docuent ,the text between 3 and 10 index
+console.log("the text between 3 and 10 index is:",text.slice(3,10));
+document.write("the text between 3 and 10 index is=>",text.slice(3,10));
+
+console.log(document);

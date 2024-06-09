b958fe1a9ef89e7ef2944b4cd41e682480a0262b
sirjana <tsirjana059@gmail.com>
2024-06-09 08:18:56 +0545

    arraymethod

---
+++ Array.js
@@ -0,0 +1,7 @@
+var fruits=["banana","orange","apple","mango"]
+// length of array
+console.log("returs the length of array",fruits.length);
+// converts an array to a string of (comma separated) array values.
+console.log("converts an array to string",fruits.toString());
+//get the third element of fruits using at()
+console.log("get the third element of fruits",fruits.at(2));
---
+++ numberMethod.js
@@ -0,0 +1,6 @@
+var xyz=1214234;
+//convert number to string
+console.log("converting the number to string",xyz.toString());
+console.log("returns the sting with number roundoff",xyz.toExponential(2));
+console.log("returns a string eith specifed number",xyz.toFixed(2));
+console.log("returns the string with specified number length".xyz.toPrecision(2));

8377b127847643ee1ec7519732ac9675d9603cdb
sirjana <tsirjana059@gmail.com>
2024-06-18 08:12:20 +0545

    conditional statement

--- Array.js
+++ Array.js
@@ -5,3 +5,4 @@ console.log("returs the length of array",fruits.length);
 console.log("converts an array to string",fruits.toString());
 //get the third element of fruits using at()
 console.log("get the third element of fruits",fruits.at(2));
+console.log("get the third element of fruits",fruits[2]);
---
+++ condtion.js
@@ -0,0 +1,20 @@
+//show the num is greater or less than 50
+var num=40;
+if(num==50){
+    console.log("num is equal to 50");
+}
+else if(num<=50){
+    console.log ("number is less than 50");
+}
+else{
+    console.log("num is greater than 50");
+}
+
+// show the given no is even
+var num=4;
+if(num%2==0){
+    console .log("number is even");
+}
+else{
+    console.log("number is odd");
+}
